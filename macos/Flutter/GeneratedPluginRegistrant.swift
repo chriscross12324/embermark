d3fe022b80b48874ebec63016b6375006c3eb7dc
Chris Coulthard <t00661081@mytru.ca>
2025-04-14 20:36:23 -0700

    - Updated display name on macOS. -> Updated pubspec.yaml; Added dependency 'macos_haptic_feedback' (Likely to remove later).

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import bitsdojo_window_macos
+import macos_haptic_feedback
 import macos_window_utils
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BitsdojoWindowPlugin.register(with: registry.registrar(forPlugin: "BitsdojoWindowPlugin"))
+  MacosHapticFeedbackPlugin.register(with: registry.registrar(forPlugin: "MacosHapticFeedbackPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
