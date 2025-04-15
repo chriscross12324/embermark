2a594ec5f508588d8880379d972f602e5cd033cb
Chris Coulthard <t00661081@mytru.ca>
2025-04-15 16:34:11 -0700

    - Updated custom_dropdown_button.dart & custom_icon_button.dart; Added subtle scale animation on pressed down.

--- lib/widgets/custom_dropdown_button.dart
+++ lib/widgets/custom_dropdown_button.dart
@@ -42,62 +42,67 @@ class _CustomDropdownButtonState extends State<CustomDropdownButton> {
             _isPressed = false;
           });
         },
-        child: AnimatedContainer(
-          height: 40,
-          width: 120,
-          decoration: BoxDecoration(
-            color:
-                _isPressed
-                    ? Color(0xFF3D3D3D)//0xFF343434
-                    : _isHovered
-                    ? Color(0xFF424242)
-                    : Color(0xFF343434),
-            borderRadius: BorderRadius.circular(12.5),
-            border: Border.all(
-              color: Colors.white.withValues(
-                alpha:
-                    _isPressed
-                        ? 0
-                        : _isHovered
-                        ? 0.05
-                        : 0,
-              ),
-              width: 1.5,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(
+        child: AnimatedScale(
+          scale: _isPressed ? 0.99 : 1.0,
+          duration: const Duration(milliseconds: 75),
+          curve: Curves.linear,
+          child: AnimatedContainer(
+            height: 40,
+            width: 120,
+            decoration: BoxDecoration(
+              color:
+                  _isPressed
+                      ? Color(0xFF3D3D3D)//0xFF343434
+                      : _isHovered
+                      ? Color(0xFF424242)
+                      : Color(0xFF343434),
+              borderRadius: BorderRadius.circular(12.5),
+              border: Border.all(
+                color: Colors.white.withValues(
                   alpha:
                       _isPressed
                           ? 0
                           : _isHovered
-                          ? 0.18
+                          ? 0.05
                           : 0,
                 ),
-                blurRadius: 5,
-                offset: Offset(0, 5),
+                width: 1.5,
               ),
-            ],
-          ),
-          duration: const Duration(milliseconds: 75),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 8, right: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'Header 1',
-                  style: GoogleFonts.nunito(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(
+                    alpha:
+                        _isPressed
+                            ? 0
+                            : _isHovered
+                            ? 0.18
+                            : 0,
                   ),
-                ),
-                HugeIcon(
-                  icon: HugeIcons.strokeRoundedArrowDown01,
-                  color: Colors.white,
-                  size: 20,
+                  blurRadius: 5,
+                  offset: Offset(0, 5),
                 ),
               ],
+            ),
+            duration: const Duration(milliseconds: 75),
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8, right: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Header 1',
+                    style: GoogleFonts.nunito(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  HugeIcon(
+                    icon: HugeIcons.strokeRoundedArrowDown01,
+                    color: Colors.white,
+                    size: 20,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/widgets/custom_icon_button.dart
+++ lib/widgets/custom_icon_button.dart
@@ -43,47 +43,53 @@ class _CustomIconButtonState extends State<CustomIconButton> {
             _isPressed = false;
           });
         },
-        child: AnimatedContainer(
-          height: 40,
-          width: 40,
-          decoration: BoxDecoration(
-            color:
-                _isPressed ? Color(0xFF3D3D3D) :
-                _isHovered
-                    ? Color(0xFF424242)
-                    : Color(0xFF424242).withValues(alpha: 0),
-            borderRadius: BorderRadius.circular(12.5),
-            border: Border.all(
-              color: Colors.white.withValues(
-                alpha:
-                    _isPressed
-                        ? 0
-                        : _isHovered
-                        ? 0.05
-                        : 0,
-              ),
-              width: 1.5,
-            ),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withValues(
+        child: AnimatedScale(
+          scale: _isPressed ? 0.99 : 1.0,
+          duration: const Duration(milliseconds: 75),
+          curve: Curves.linear,
+          child: AnimatedContainer(
+            height: 40,
+            width: 40,
+            decoration: BoxDecoration(
+              color:
+                  _isPressed ? Color(0xFF3D3D3D) :
+                  _isHovered
+                      ? Color(0xFF424242)
+                      : Color(0xFF424242).withValues(alpha: 0),
+              borderRadius: BorderRadius.circular(12.5),
+              border: Border.all(
+                color: Colors.white.withValues(
                   alpha:
                       _isPressed
                           ? 0
                           : _isHovered
-                          ? 0.18
+                          ? 0.05
                           : 0,
                 ),
-                blurRadius: 5,
-                offset: Offset(0, 5),
+                width: 1.5,
               ),
-            ],
-          ),
-          duration: const Duration(milliseconds: 75),
-          child: HugeIcon(
-            icon: widget.icon,
-            color: Colors.white,
-            size: 22,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withValues(
+                    alpha:
+                        _isPressed
+                            ? 0
+                            : _isHovered
+                            ? 0.18
+                            : 0,
+                  ),
+                  blurRadius: 5,
+                  offset: Offset(0, 5),
+                ),
+              ],
+            ),
+            duration: const Duration(milliseconds: 75),
+            curve: Curves.linear,
+            child: HugeIcon(
+              icon: widget.icon,
+              color: Colors.white,
+              size: 22,
+            ),
           ),
         ),
       ),
