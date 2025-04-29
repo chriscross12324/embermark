ea0511f2cea8877b17cdf5303d9288de99cb7bae
Chris Coulthard <t00661081@mytru.ca>
2025-04-28 20:25:49 -0700

    - Updated app_conversions.dart; Updated to include additional serialization/deserialization functions.

--- lib/core/app_conversions.dart
+++ lib/core/app_conversions.dart
@@ -15,10 +15,10 @@ String serializePadding(EdgeInsets padding) {
 EdgeInsets deserializePadding(String paddingMap) {
   final paddingJson = jsonDecode(paddingMap);
   return EdgeInsets.fromLTRB(
-    (paddingJson['left'] as num).toDouble(),
-    (paddingJson['top'] as num).toDouble(),
-    (paddingJson['right'] as num).toDouble(),
-    (paddingJson['bottom'] as num).toDouble(),
+    double.parse(paddingJson['left']),
+    double.parse(paddingJson['top']),
+    double.parse(paddingJson['right']),
+    double.parse(paddingJson['bottom']),
   );
 }
 
@@ -27,29 +27,33 @@ String serializeTextStyle(TextStyle textStyle) {
     'color': serializeColor(textStyle.color!),
     'backgroundColour': serializeColor(textStyle.backgroundColor!),
     'fontSize': textStyle.fontSize.toString(),
-    'fontWeight': textStyle.fontWeight,
-    'fontStyle': textStyle.fontStyle,
+    'fontWeight': serializeFontWeight(textStyle.fontWeight!),
+    'fontStyle': serializeEnum(textStyle.fontStyle!),
     'letterSpacing': textStyle.letterSpacing.toString(),
     'wordSpacing': textStyle.wordSpacing.toString(),
-    'textBaseline': textStyle.textBaseline,
     'height': textStyle.height.toString(),
-    'leadingDistribution': textStyle.leadingDistribution,
-    'locale': textStyle.locale,
-    'foreground': textStyle.foreground,
-    'background': textStyle.background,
-    'shadows': textStyle.shadows,
-    'fontFeatures': textStyle.fontFeatures,
-    'fontVariations': textStyle.fontVariations,
-    'decoration': textStyle.decoration,
-    'decorationColor': serializeColor(textStyle.decorationColor!),
-    'decorationStyle': textStyle.decorationStyle,
-    'decorationThickness': textStyle.decorationThickness.toString(),
+    'shadows': serializeShadows(textStyle.shadows!),
   };
   return jsonEncode(textStyleMap);
 }
 
 TextStyle deserializeTextStyle(String textStyleMap) {
-  return TextStyle();
+  final textStyleJson = jsonDecode(textStyleMap);
+  return TextStyle(
+    color: deserializeColor(textStyleJson['color']),
+    backgroundColor: deserializeColor(textStyleJson['backgroundColour']),
+    fontSize: double.parse(textStyleJson['fontSize']),
+    fontWeight: deserializeFontWeight(textStyleJson['fontWeight']),
+    fontStyle: deserializeEnum(
+      textStyleJson['fontStyle'],
+      FontStyle.values,
+      fallback: FontStyle.normal,
+    ),
+    letterSpacing: double.parse(textStyleJson['letterSpacing']),
+    wordSpacing: double.parse(textStyleJson['wordSpacing']),
+    height: double.parse(textStyleJson['height']),
+    shadows: deserializeShadows(textStyleJson['shadows']),
+  );
 }
 
 String serializeColor(Color colour) {
@@ -64,24 +68,90 @@ String serializeColor(Color colour) {
 
 Color deserializeColor(String colourMap) {
   final colourJson = jsonDecode(colourMap);
-  return Color.fromARGB(
-    colourJson['a'],
-    colourJson['r'],
-    colourJson['g'],
-    colourJson['b'],
+  return Color.from(
+    alpha: colourJson['a'],
+    red: colourJson['r'],
+    green: colourJson['g'],
+    blue: colourJson['b'],
   );
 }
 
 String serializeFontWeight(FontWeight fontWeight) {
-  final fontWeightMap = {
-    'index': fontWeight.index,
-    'value': fontWeight.value,
-  };
-  return jsonEncode(fontWeightMap);
+  return jsonEncode({'value': fontWeight.value});
 }
 
 FontWeight deserializeFontWeight(String fontWeightMap) {
   final fontWeightJson = jsonDecode(fontWeightMap);
-  final fontWeight = FontWeight._()
-  return FontWeight(
+  return FontWeight.values.firstWhere(
+    (fw) => fw.value == fontWeightJson['value'],
+    orElse: () => FontWeight.normal,
+  );
+}
+
+String serializeShadows(List<Shadow> shadowList) {
+  final List<String> shadowListMap = [];
+  for (final shadow in shadowList) {
+    final shadowMap = serializeShadow(shadow);
+    shadowListMap.add(jsonEncode(shadowMap));
+  }
+  return jsonEncode(shadowListMap);
+}
+
+List<Shadow> deserializeShadows(String shadowListMap) {
+  final shadowListJson = jsonDecode(shadowListMap);
+  final shadowList = List<Map<String, dynamic>>.from(
+    shadowListJson.map((item) => Map<String, dynamic>.from(item)),
+  );
+
+  return [Shadow()];
+  /*List<Shadow> shadows = [];
+  for (final shadow in shadowList) {
+    final shadowObject = deserializeShadow(shadow);
+  }*/
+}
+
+String serializeShadow(Shadow shadow) {
+  final shadowMap = {
+    'color': serializeColor(shadow.color),
+    'offset': serializeOffset(shadow.offset),
+    'blurRadius': shadow.blurRadius.toString(),
+  };
+  return jsonEncode(shadowMap);
+}
+
+Shadow deserializeShadow(String shadowMap) {
+  final shadowJson = jsonDecode(shadowMap);
+  return Shadow(
+    color: deserializeColor(shadowJson['color']),
+    offset: deserializeOffset(shadowJson['offset']),
+    blurRadius: double.parse(shadowJson['blurRadius']),
+  );
+}
+
+String serializeOffset(Offset offset) {
+  final offsetMap = {'dx': offset.dx.toString(), 'dy': offset.dy.toString()};
+  return jsonEncode(offsetMap);
+}
+
+Offset deserializeOffset(String offsetMap) {
+  final offsetJson = jsonDecode(offsetMap);
+  return Offset(double.parse(offsetJson['dx']), double.parse(offsetJson['dy']));
+}
+
+String serializeEnum(Enum e) {
+  return jsonEncode({'name': e.name});
+}
+
+T deserializeEnum<T extends Enum>(
+  String enumMap,
+  List<T> values, {
+  required T fallback,
+}) {
+  final enumJson = jsonDecode(enumMap);
+  if (enumJson['name'] == null) return fallback;
+
+  return values.firstWhere(
+    (e) => e.name == enumJson['name'],
+    orElse: () => fallback,
+  );
 }
