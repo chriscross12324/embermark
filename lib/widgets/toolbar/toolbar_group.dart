ddd597a03f0f17aa70471b62c5632be4a8dee2db
Chris Coulthard <t00661081@mytru.ca>
2025-05-04 18:49:58 -0700

    - Added toolbar_group.dart; Makes it easy to group similar tools together in the toolbar.

---
+++ lib/widgets/toolbar/toolbar_group.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ToolbarGroup extends StatefulWidget {
+  const ToolbarGroup({
+    super.key,
+    required this.children,
+    required this.groupName,
+  });
+
+  final List<Widget> children;
+  final String groupName;
+
+  @override
+  State<ToolbarGroup> createState() => _ToolbarGroupState();
+}
+
+class _ToolbarGroupState extends State<ToolbarGroup> {
+  final LayerLink _layerLink = LayerLink();
+  final GlobalKey _key = GlobalKey();
+  OverlayEntry? _overlayEntry;
+  bool _menuVisible = false;
+
+  void _toggleMenu() {
+    if (_menuVisible) {
+      _hideMenu();
+    } else {
+      _showMenu();
+    }
+  }
+
+  void _showMenu() {
+    _overlayEntry = OverlayEntry(
+      builder:
+          (context) => Positioned(
+            width: 300,
+            child: CompositedTransformFollower(
+              link: _layerLink,
+              offset: const Offset(0, -50),
+              showWhenUnlinked: false,
+              child: Material(
+                elevation: 4,
+                borderRadius: BorderRadius.circular(8),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: widget.children,
+                ),
+              ),
+            ),
+          ),
+    );
+
+    Overlay.of(context).insert(_overlayEntry!);
+    setState(() => _menuVisible = true);
+  }
+
+  void _hideMenu() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+    setState(() => _menuVisible = false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(key: _key, children: widget.children);
+  }
+
+  @override
+  void dispose() {
+    _hideMenu();
+    super.dispose();
+  }
+}
